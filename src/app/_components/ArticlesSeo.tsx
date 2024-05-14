b5153418ff5a44e2e313053393b096faa8362da6
Valkon <96625039+Nawija@users.noreply.github.com>
2024-05-14 20:24:35 +0200

    Update ArticlesSeo.tsx

--- src/app/_components/ArticlesSeo.tsx
+++ src/app/_components/ArticlesSeo.tsx
@@ -4,16 +4,17 @@ import s1 from "../../../public/sandy-millar-8vaQKYnawHw-unsplash.jpg";
 export default function ArticlesSeo() {
     return (
         <div>
-            <section className="max-w-screen-2xl mx-auto space-y-8">
-                <h2 className="text-center text-3xl font-bold max-w-screen-lg mx-auto my-12 lg:my-20">
+            <section className="max-w-screen-xl mx-auto space-y-8">
+                <h2 className="text-center text-2xl lg:text-3xl font-bold max-w-screen-lg mx-auto my-12 lg:my-20">
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                     Distinctio quo perferendis, dolores totam
                 </h2>
                 <div className="flex lg:flex-row lg:my-24 flex-col items-center justify-center">
-                    <div className="relative lg:w-1/2 w-11/12 h-full">
+                    <div className="relative w-full h-96">
+                        <div className="bg-gradient-to-l from-white via-transparent to-white h-full w-full absolute left-0 top-0 z-10" />
                         <Image
-                            className="h-full w-full"
                             src={s1}
+                            className="h-full w-full object-cover"
                             layout="fill"
                             alt="..."
                         />
@@ -42,12 +43,17 @@ export default function ArticlesSeo() {
                     </div>
                 </div>
             </section>
-            <section className="max-w-screen-2xl mx-auto space-y-8">
+            <section className="max-w-screen-xl mx-auto space-y-8">
                 <div className="flex lg:flex-row lg:my-24 flex-col items-center justify-center">
-                    <img
-                        className="lg:w-1/2 w-11/12 lg:order-1"
-                        src="https://plus.unsplash.com/premium_photo-1670985849616-6aa6c441e0bf?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    />
+                    <div className="relative w-full h-96 order-1">
+                        <div className="bg-gradient-to-l from-white via-transparent to-white h-full w-full absolute left-0 top-0 z-10" />
+                        <Image
+                            src={s1}
+                            className="h-full w-full object-cover"
+                            layout="fill"
+                            alt="..."
+                        />
+                    </div>
                     <div className="p-10 space-y-4">
                         <h3 className="text-3xl font-bold">
                             Lorem ipsum dolor sit amet consectetur adipisicing
@@ -72,12 +78,17 @@ export default function ArticlesSeo() {
                     </div>
                 </div>
             </section>
-            <section className="max-w-screen-2xl mx-auto space-y-8">
+            <section className="max-w-screen-xl mx-auto space-y-8">
                 <div className="flex lg:flex-row lg:my-24 flex-col items-center justify-center">
-                    <img
-                        className="lg:w-1/2 w-11/12"
-                        src="https://plus.unsplash.com/premium_photo-1670985849616-6aa6c441e0bf?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    />
+                    <div className="relative w-full h-96">
+                        <div className="bg-gradient-to-l from-white via-transparent to-white h-full w-full absolute left-0 top-0 z-10" />
+                        <Image
+                            src={s1}
+                            className="h-full w-full object-cover"
+                            layout="fill"
+                            alt="..."
+                        />
+                    </div>
                     <div className="p-10 space-y-4">
                         <h3 className="text-3xl font-bold">
                             Lorem ipsum dolor sit amet consectetur adipisicing
