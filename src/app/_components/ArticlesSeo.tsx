43ab99346e41082cabf4cec58343ab67acce80f7
Valkon <96625039+Nawija@users.noreply.github.com>
2024-05-14 13:08:58 +0200

    dgf

--- src/app/_components/ArticlesSeo.tsx
+++ src/app/_components/ArticlesSeo.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import s1 from "../../../public/sandy-millar-8vaQKYnawHw-unsplash.jpg";
+
 export default function ArticlesSeo() {
     return (
         <div>
@@ -7,9 +10,11 @@ export default function ArticlesSeo() {
                     Distinctio quo perferendis, dolores totam
                 </h2>
                 <div className="flex lg:flex-row lg:my-24 flex-col items-center justify-center">
-                    <img
+                    <Image
                         className="lg:w-1/2 w-11/12"
-                        src="https://plus.unsplash.com/premium_photo-1670985849616-6aa6c441e0bf?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                        src={s1}
+                        layout="fill"
+                        alt="..."
                     />
                     <div className="p-10 space-y-4">
                         <h3 className="text-3xl font-bold">
