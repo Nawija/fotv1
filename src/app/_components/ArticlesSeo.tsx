41c21863bb5fa3e723429e2c9bff9b9e49a93899
Valkon <96625039+Nawija@users.noreply.github.com>
2024-05-15 13:59:52 +0200

    gdf

--- src/app/_components/ArticlesSeo.tsx
+++ src/app/_components/ArticlesSeo.tsx
@@ -1,118 +1,61 @@
 import Image from "next/image";
-import s1 from "../../../public/sandy-millar-8vaQKYnawHw-unsplash.jpg";
+import s1 from "../../../public/camera-7903435_1280.webp";
+
+const data = [
+    {
+        img: s1,
+        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit Fugiat esse minima voluptatem corporis tequod aspernatur voluptatum exercitationem et, optio tenetur distinctio illum temporibus maxime culpa eligendi quasi. Non optio accusamus molestias nobis! Aspernatur, omnis nisi. Repellat repellendus deleniti dignissimos fugit accusantium minus voluptatibus.",
+        desc2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora debitis vitae asperiores recusandae esse, quod aspernatur voluptatum exercitationem et, optio tenetur distinctioatur, omnis nisi. Repellat repellendus deleniti dignissimos fugit accusantium minus voluptatibus.",
+    },
+    {
+        img: s1,
+        title: "Lorem ipsum dolor sit amet consectetur adipisicing elit Fugiat esse minima voluptatem corporis tempora, totam, laboriosam laborum itaque",
+        desc1: "Lorem ipsum dolor sit amet, consectr, omnis nisi. Repellat repellendus deleniti dignissimos fugit accusantium minus voluptatibus.",
+        desc2: "bus maxime culpa eligendi quasi. Non optio accusamus molestias nobis! Aspernatur, omnis nisi. Repellat repellendus deleniti dignissimos fugit accusantium minus voluptatibus.",
+    },
+    {
+        img: s1,
+        title: "elit Fugiat esse minima voluptatem corporis tempora, totam, laboriosam laborum itaque",
+        desc1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora debitis vitae asperiores recusandae esse, quod aspernatur voluptatum exercitationem et, optio tenetur distinctio illum temporibus maxime culpa eligendi quasi. Non optio accusamus molestias nobis! Aspernatur, omnis nisi. Repellat repellendus deleniti dignissimos fugit accusantium minus voluptatibus.",
+        desc2: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora debitis vitae asperiores recusandae esse, quod aspernatur voluptatum exercitationem et, optio tenetur distinctio illum tems nisi. Repellat repellendus deleniti dignissimos fugit accusantium minus voluptatibus.",
+    },
+];
 
 export default function ArticlesSeo() {
     return (
-        <div>
-            <section className="max-w-screen-xl mx-auto space-y-8">
-                <h2 className="text-center text-2xl lg:text-3xl font-bold max-w-screen-lg mx-auto my-12 lg:my-20">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Distinctio quo perferendis, dolores totam
-                </h2>
-                <div className="flex lg:flex-row lg:my-24 flex-col items-center justify-center">
-                    <div className="relative w-full h-96">
-                        <div className="bg-gradient-to-l from-white via-transparent to-white h-full w-full absolute left-0 top-0 z-10" />
-                        <Image
-                            src={s1}
-                            className="h-full w-full object-cover"
-                            layout="fill"
-                            alt="..."
-                        />
+        <>
+            <h2 className="text-center text-2xl lg:text-3xl font-bold max-w-screen-lg mx-auto my-12 lg:my-20 px-4">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Distinctio quo perferendis, dolores totam
+            </h2>
+            {data.map((article, index) => (
+                <section
+                    key={index}
+                    className="max-w-screen-xl mx-auto space-y-8"
+                >
+                    <div className="flex lg:flex-row lg:my-24 flex-col items-center justify-center">
+                        <div
+                            className={`relative w-full lg:w-96 h-96 ${
+                                index % 2 ? "order-1" : "order-0"
+                            }`}
+                        >
+                            <Image
+                                src={s1}
+                                className="h-full w-full object-cover"
+                                layout="fill"
+                                alt="..."
+                            />
+                        </div>
+                        <div className="p-10 space-y-4">
+                            <h3 className="text-3xl font-bold">
+                                {article.title}
+                            </h3>
+                            <p>{article.desc1}</p>
+                            <p>{article.desc2}</p>
+                        </div>
                     </div>
-                    <div className="p-10 space-y-4">
-                        <h3 className="text-3xl font-bold">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Fugiat esse minima voluptatem corporis
-                            tempora, totam, laboriosam laborum itaque
-                        </h3>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Tempora debitis vitae asperiores recusandae
-                            esse, quod aspernatur voluptatum exercitationem et,
-                            optio tenetur distinctio illum temporibus maxime
-                            culpa eligendi quasi. Non optio accusamus molestias
-                            nobis! Aspernatur, omnis nisi. Repellat repellendus
-                            deleniti dignissimos fugit accusantium minus
-                            voluptatibus.
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Tempora debitis vitae asperiores recusandae
-                            esse, quod mos fugit accusantium minus voluptatibus.
-                        </p>
-                    </div>
-                </div>
-            </section>
-            <section className="max-w-screen-xl mx-auto space-y-8">
-                <div className="flex lg:flex-row lg:my-24 flex-col items-center justify-center">
-                    <div className="relative w-full h-96 order-1">
-                        <div className="bg-gradient-to-l from-white via-transparent to-white h-full w-full absolute left-0 top-0 z-10" />
-                        <Image
-                            src={s1}
-                            className="h-full w-full object-cover"
-                            layout="fill"
-                            alt="..."
-                        />
-                    </div>
-                    <div className="p-10 space-y-4">
-                        <h3 className="text-3xl font-bold">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Fugiat esse minima voluptatem corporis
-                            tempora, totam, laboriosam laborum itaque
-                        </h3>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Tempora debitis vitae asperiores recusandae
-                            esse, quod aspernatur voluptatum exercitationem et,
-                            optio tenetur distinctio illum temporibus maxime
-                            culpa eligendi quasi. Non optio accusamus molestias
-                            nobis! Aspernatur, omnis nisi. Repellat repellendus
-                            deleniti dignissimos fugit accusantium minus
-                            voluptatibus.
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Tempora debitis vitae asperiores recusandae
-                            esse, quod mos fugit accusantium minus voluptatibus.
-                        </p>
-                    </div>
-                </div>
-            </section>
-            <section className="max-w-screen-xl mx-auto space-y-8">
-                <div className="flex lg:flex-row lg:my-24 flex-col items-center justify-center">
-                    <div className="relative w-full h-96">
-                        <div className="bg-gradient-to-l from-white via-transparent to-white h-full w-full absolute left-0 top-0 z-10" />
-                        <Image
-                            src={s1}
-                            className="h-full w-full object-cover"
-                            layout="fill"
-                            alt="..."
-                        />
-                    </div>
-                    <div className="p-10 space-y-4">
-                        <h3 className="text-3xl font-bold">
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Fugiat esse minima voluptatem corporis
-                            tempora, totam, laboriosam laborum itaque
-                        </h3>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Tempora debitis vitae asperiores recusandae
-                            esse, quod aspernatur voluptatum exercitationem et,
-                            optio tenetur distinctio illum temporibus maxime
-                            culpa eligendi quasi. Non optio accusamus molestias
-                            nobis! Aspernatur, omnis nisi. Repellat repellendus
-                            deleniti dignissimos fugit accusantium minus
-                            voluptatibus.
-                        </p>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing
-                            elit. Tempora debitis vitae asperiores recusandae
-                            esse, quod mos fugit accusantium minus voluptatibus.
-                        </p>
-                    </div>
-                </div>
-            </section>
-        </div>
+                </section>
+            ))}
+        </>
     );
 }
--- src/components/HeaderInfo.tsx
+++ src/components/HeaderInfo.tsx
@@ -6,23 +6,20 @@ import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function HeaderInfo() {
-    const pathname = usePathname();
     return (
-        pathname === "/" && (
-            <div
-                className={`${bitter.className} anim-opacity bg-black text-white text-xs font-light px-4 py-2`}
-            >
-                <div className="max-w-[1900px] items-center justify-end mx-auto w-full flex space-x-6">
-                    <div className="flex items-center justify-center space-x-1">
-                        <Mail size={16} />
-                        <span>jarekolszewski@gmail.com</span>
-                    </div>
-                    <div className="flex items-center justify-center space-x-1">
-                        <Phone size={16} />
-                        <span>+48 500-233-356</span>
-                    </div>
+        <div
+            className={`${bitter.className} bg-black text-white text-xs font-light px-4 py-2`}
+        >
+            <div className="max-w-[1900px] items-center justify-end mx-auto w-full flex space-x-6">
+                <div className="flex items-center justify-center space-x-1">
+                    <Mail size={16} />
+                    <span>jarekolszewski@gmail.com</span>
+                </div>
+                <div className="flex items-center justify-center space-x-1">
+                    <Phone size={16} />
+                    <span>+48 500-233-356</span>
                 </div>
             </div>
-        )
+        </div>
     );
 }
